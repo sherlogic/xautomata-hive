68478787e19ba89422db90cc11e952370212f718
ajacassi <a.jacassi@vargroup.it>
2023-08-04 09:50:14 +0200

    aggiunto un endpoint delle probe

--- hive/cookbook/probes.py
+++ hive/cookbook/probes.py
@@ -102,3 +102,32 @@ class Probes(ApiManager):
         if kwargs is None: kwargs = dict()
         response = self.execute('DELETE', path=f'/probes/{uuid}', **kwargs)
         return response
+
+    def probes_objects(self, uuid: str, single_page: bool = False, page_size: int = 5000, warm_start: bool = False,
+                       kwargs: dict = None, **params):
+        """
+
+        fetch objects releted to a probe.
+
+        Args:
+            uuid: id del probe
+            single_page (bool, optional): se False la risposta viene ottenuta a step per non appesantire le API. Default to False.
+            page_size (int, optional): Numero di oggetti per pagina se single_page == False. Default to 5000.
+            warm_start (bool, optional): salva la risposta in un file e se viene richiamata la stessa funzione con gli stessi argomenti restituisce il contenuto del file. Default to False.
+            kwargs (dict, optional): additional parameters for execute. Default to None.
+            **params: additional parameters for the API
+
+        Keyword Args:
+            not_in (bool, opional): additional filter
+            skip (int, optional): numero di oggetti che si vogliono saltare nella risposta. Default to 0.
+            limit (int, optional): numero di oggetti massimi che si vogliono ottenere. Default to 1_000_000.
+            count (bool, optional): Se True nel header della risposta e' presente la dimensione massima a db della chiamata fatta, sconsigliabile perche raddoppia il tempo per chiamata. Default to False.
+            like (bool, optional): Se True, eventuali filtri richiesti dalla API vengono presi come porzioni di testo, se False il matching sul campo dei filtri deve essere esatto. Default to True.
+            join (bool, optional): Se join = true, ogni riga restituita conterrà chiavi aggiuntive che fanno riferimento ad altre entità, con cui la riga ha relazioni 1:1. Default to False
+
+        Returns: list
+        """
+        if kwargs is None: kwargs = dict()
+        response = self.execute('GET', path=f'/probes/{uuid}/objects', single_page=single_page, page_size=page_size,
+                                warm_start=warm_start, params=params, **kwargs)
+        return response
