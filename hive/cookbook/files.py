bf8d5742d9d4f8acb77e8e14be48de98c50ac1a4
github-build-hive-actions <github-actions[bot]@users.noreply.github.com>
2025-09-22 17:20:23 +0200

    Update Hive docs from Github Actions

--- hive/cookbook/files.py
+++ hive/cookbook/files.py
@@ -70,6 +70,22 @@ class Files(ApiManager):
             payload=payload, **kwargs)
         return response
 
+    def file(self, uuid: str, warm_start: bool = False, kwargs: dict = None
+        ) -> list:
+        """Read User File
+
+        Args:
+            uuid (str, required): uuid
+            warm_start (bool, optional): salva la risposta in un file e se viene richiamata la stessa funzione con gli stessi argomenti restituisce il contenuto del file. Default to False.
+            kwargs (dict, optional): additional parameters for execute. Default to None.
+
+        Returns: list"""
+        if kwargs is None:
+            kwargs = dict()
+        response = self.execute('GET', path=f'/files/{uuid}', warm_start=
+            warm_start, **kwargs)
+        return response
+
     def files_download(self, uuid: str, warm_start: bool = False,
         kwargs: dict = None) -> list:
         """Download User File
